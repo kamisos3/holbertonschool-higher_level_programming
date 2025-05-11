c70ada15868dd2fca4229e666245d9913b7374dc
kamisos3 <kamilasostre11@gmail.com>
2025-05-11 17:26:33 -0400

    Printing alphabet with new line

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123): # Prints ascii value
-    print(chr(i), end=" ")
+for i in range(97, 123): #ASCII value of lowercase alphabet
+    print(chr(i), end="")
+print()
