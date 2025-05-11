7b60cdc65b6a215d1dbe85005124a866efd73ea5
kamisos3 <kamilasostre11@gmail.com>
2025-05-11 16:33:09 -0400

    Function that assigns a random number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if number > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 if number == 0:
     print(f"Last digit of {number} is 0 and is 0")
-if number < 6 | number > 0:
+if number < 6:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
