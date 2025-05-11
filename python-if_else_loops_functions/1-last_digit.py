da85d20810a7007916aef1bda75fb9c7ac34747f
kamisos3 <kamilasostre11@gmail.com>
2025-05-11 16:45:36 -0400

    Renaming variables so they properly print

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10 if number >= 0 else -(-number % 10)
 
-if number > 5:
+if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-if number == 0:
+elif last_digit == 0:
     print(f"Last digit of {number} is 0 and is 0")
-if number < 6:
+else:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
