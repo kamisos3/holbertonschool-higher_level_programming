b84107480602d8c0de0d6eecad9c2dc980332a59
kamisos3 <kamilasostre11@gmail.com>
2025-05-16 16:16:35 -0400

    Update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument:")
     else:
-        print(f"{count} arguments")
+        print(f"{count} arguments:")
 
     for i, arg in enumerate(args, start=1):
         print(f"{i}: {args[i-1]}")
