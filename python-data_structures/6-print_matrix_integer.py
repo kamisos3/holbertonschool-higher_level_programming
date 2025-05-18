0ae904646a70f62c9c9abb45e9e089b1770b9e68
kamisos3 <kamilasostre11@gmail.com>
2025-05-18 17:37:15 -0400

    Making a function that prints a matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i != len(row) - 1:
-                print("{:d}".format(row[i], end=" "))
+        if not row:
+            print()
+            continue
+        for i, value in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(value), end=" ")
             else:
-                print("{:d}".format(row[i], end=""))
-        print()
+                print("{:d}".format(value))
