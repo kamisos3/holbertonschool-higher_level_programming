56638d98339946dd25bc3b59147aab65455dc7b5
kamisos3 <kamilasostre11@gmail.com>
2025-05-19 12:44:06 -0400

    Adding function

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,9 +3,10 @@ def common_elements(set_1, set_2):
     """
     Return z set containing the common elements in two sets.
     """
-    return set_1 & set_2 # Using set intersection
+    return set_1 & set_2   # Using set intersection
+
 
 if __name__ == "__main__":
     set_a = {"Python", "C", "Javascript"}
     set_b = {"Bash", "C", "Ruby", "Perl"}
-    print(common_elements(set_a, set_b)) # Expected output: {'C'}
+    print(common_elements(set_a, set_b))   # Expected output: {'C'}
