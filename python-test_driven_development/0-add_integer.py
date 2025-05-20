be5388e4c71956391eeff6dd02b4e1a2dc7e64f3
kamisos3 <kamilasostre11@gmail.com>
2025-05-20 18:11:25 -0400

    Adding a function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,4 +10,13 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    
+
+    try:
+        a = int(a)
+        b = int(b)
+    except OverflowError:
+        raise OverflowError("cannot convert a float infinty to integer")
+    
+
+    return a + b
