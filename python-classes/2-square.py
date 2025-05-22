de682b9204bd627efe23aec9e1f99965b227aa79
kamisos3 <kamilasostre11@gmail.com>
2025-05-22 19:57:50 -0400

    Inserting private size attribute to square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,22 +3,13 @@
 
 
 class Square:
-    """Defines a  square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-
-def __init__(self, size=0):
-    """Initialize square with private size attribute
-
-    Args:
-        size (int): The size of the square. Defaults to 0.
-
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-    self.__size = size   # Private attribute
+    @property
+    def size(self):
+        return self.__size
