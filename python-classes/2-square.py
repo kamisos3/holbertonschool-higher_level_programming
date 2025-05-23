6c2b72458a71681b2f9084f28d110e9897d804da
kamisos3 <kamilasostre11@gmail.com>
2025-05-22 20:28:46 -0400

    Updating function

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,10 @@
 class Square:
     """Defines square"""
 
-
     def __init__(self, size=0):
         """Initialize square with given size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
