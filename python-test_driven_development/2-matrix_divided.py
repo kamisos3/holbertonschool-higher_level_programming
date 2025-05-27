2179f8054e2cd3f32f369003ba6919540d9cdc54
kamisos3 <kamilasostre11@gmail.com>
2025-05-26 20:37:29 -0400

    Updating function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,13 +6,16 @@ This module defines a function to divide elements of a matrix.
 
 def matrix_divided(matrix, div):
     """Divides elements of a matrix by divisor"""
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance
+        (matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError
+        ("matrix must be a matrix (list of lists) of integers/floats")
 
     for row in matrix:
         for elem in row:
             if not isinstance(elem, (int, float)) or isinstance(elem, bool):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError
+                ("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
