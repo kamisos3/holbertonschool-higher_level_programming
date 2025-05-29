9d9a5a28ad61d5d79a63b24f285284ea5f83b132
kamisos3 <kamilasostre11@gmail.com>
2025-05-29 15:01:41 -0400

    Making a class that inherits a list, TDD to be done

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Method that defines a class that inherits"""
+
+
+class MyList(list):
+    """This class inherits from list"""
+
+    def print_sorted(self):
+        """Prints the list sorted ascending"""
+        print_sorted = sorted(self)
+        print(sorted(self))
