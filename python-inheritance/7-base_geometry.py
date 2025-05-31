08689168cdd4c414bd68b709c1ea882d436321f9
kamisos3 <kamilasostre11@gmail.com>
2025-05-31 18:29:52 -0400

    Function that raises exceptions to print error of the execution

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module raises exceptions within the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines class BaseGeometry and instance methods"""
+
+    def integer(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
