a70cbe5db8fcafb2826ff095105f49fd35d27ec6
kamisos3 <kamilasostre11@gmail.com>
2025-06-02 18:55:20 -0400

    Updating function

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module defines abstrac class"""
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """Defines Animal class"""
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """Defines Dog class"""
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """Defines Cat class"""
+    def sound(self):
+        return "Meow"
