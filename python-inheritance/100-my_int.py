de003c8dcdaf76256377734f019b370a8e4f0261
kamisos3 <kamilasostre11@gmail.com>
2025-06-03 10:31:10 -0400

    Making a class that inherits from integer object

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -4,4 +4,8 @@
 
 class MyInt(int):
     """Class MyInt is defined here"""
-    pass
+    def __eq__(self, other):  # __eq__ checks == and overrides to print !=
+        return super().__ne__(other)
+
+    def __ne__(self, other):  # __ne__ checks != and overrides to print ==
+        return super().__eq__(other)
