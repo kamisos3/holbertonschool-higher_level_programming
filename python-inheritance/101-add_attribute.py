032a6cd9cdf53132efd7a208449a5a4b6532ad1b
kamisos3 <kamilasostre11@gmail.com>
2025-06-03 11:47:51 -0400

    Checks if you can add a new attribute to an object

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""Module aads a new attribute to object if possible"""
+
+
+def add_attribute(obj, name, value):
+    """Defines add_attribute to call main"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
