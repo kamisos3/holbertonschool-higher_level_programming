e0c369df4891316cf00f8f1ae67c8ef03c56159e
kamisos3 <kamilasostre11@gmail.com>
2025-06-05 22:38:22 -0400

    Appending a string to a file so it can return the characters added

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module appends a string to the end of the file and returns # of characters added"""
+
+
+def append_write(filename="", text=""):
+    """Appends string to the file to count characters added"""
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
