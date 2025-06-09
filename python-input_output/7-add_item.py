b70b6ef23b0d820f8dc72f909055252209cbc582
kamisos3 <kamilasostre11@gmail.com>
2025-06-09 17:52:24 -0400

    Adds arguments to Python list and then saves them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module adds script with arguments to list"""
+import sys
+import os
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+# Add arguments to the list
+
+items.extend(sys.argv[1:])
+# Saves list to file
