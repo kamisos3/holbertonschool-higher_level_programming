db1cb79ea51bb6e5d8d5d4d726fbda49517b5ff8
kamisos3 <kamilasostre11@gmail.com>
2025-06-09 20:54:14 -0400

    Defines Student based on task 9

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -12,6 +12,8 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        if isinstance(attrs, list) and
+        all(isinstance(attr, str) for attr in attrs):
+            return
+            {key: getattr(self, key) for key in attrs if hasattr(self, key)}
         return self.__dict__
