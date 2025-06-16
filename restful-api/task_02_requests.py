98f5841c4a2e9b7ab9fc8df82fc7d66d4b7c66d0
kamisos3 <kamilasostre11@gmail.com>
2025-06-16 15:35:52 -0400

    Processing from API

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+import requests
+import csv
 
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("status Code:", response.status_code)
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in response.json()]
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts)
