ea7ae91f7326392dd5299bfd7a5f9cb4b309057f
kamisos3 <kamilasostre11@gmail.com>
2025-06-24 14:18:05 -0400

    Serialization functions

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,6 +8,6 @@ def serialize_and_save_to_file(data, filename):
     with open(filename, 'w') as file:
         json.dump(data, file)
 
-def load_and_desirialize(filename):
+def load_and_deserialize(filename):
     with open(filename, 'r') as file:
-        return json.load(file)
+       return json.load(file)
