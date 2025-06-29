dde43cc80876216874189ea80a24c31d5b700345
kamisos3 <kamilasostre11@gmail.com>
2025-06-29 00:30:53 -0400

    Creating user with password and all privileges granted

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
 -- Creates server user with password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Realoads user privileges from grant tables
+FLUSH PRIVILEGES;
