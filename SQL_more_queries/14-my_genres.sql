7e5cf7eb324307285178cdfa11d04f574d8e0cec
kamisos3 <kamilasostre11@gmail.com>
2025-06-29 14:13:04 -0400

    Showing all comedy shows

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,11 +5,11 @@ FROM
 	tv_shows
 JOIN
 	tv_show_genres
-   ON
+ON
 	tv_shows.id = tv_show_genres.show_id
 JOIN
 	tv_genres
-   ON
+ON
 	tv_show_genres.genre_id = tv_genres.id
 WHERE
 	tv_shows.title = 'Dexter'
