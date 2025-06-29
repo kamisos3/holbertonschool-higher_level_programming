a0920b546a5ddaf52094a7da0e021252020ff5cd
kamisos3 <kamilasostre11@gmail.com>
2025-06-29 13:16:11 -0400

    Outputs tv shows that have no genre

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Shows tv shows with no genre
-SELECT tv_shows.title, tv_shows.id = tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
