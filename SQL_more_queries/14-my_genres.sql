90fe115ada0fceaa5fe274c4adba529bd593661f
kamisos3 <kamilasostre11@gmail.com>
2025-06-29 13:50:26 -0400

    Showing all genres of specific show

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,17 @@
+-- Lists all the genres of tv show Dexter
+SELECT
+	tv_genres.name
+FROM
+	tv_shows
+JOIN
+	tv_shows_genres
+   ON
+	tv_shows.id = tv_show_genres.show_id
+JOIN
+	tv_genres
+   ON
+	tv_show_genres.genre_id = tv_show_genres.show_id
+WHERE
+	tv_shows.title = 'Dexter'
+ORDER BY
+	tv_genres.name ASC;
