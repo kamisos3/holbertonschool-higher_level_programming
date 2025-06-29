f9fdbe231c5f8f5f5e79dedc5c17da7b411ce1f5
kamisos3 <kamilasostre11@gmail.com>
2025-06-29 11:13:27 -0400

    Creates database and user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Creates a database and its user with perimissions granted
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- Creating table with id default value 1
+
