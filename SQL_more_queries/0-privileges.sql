cea4f71987fc0f30f55f31f1ec1efea4899cb0e7
kamisos3 <kamilasostre11@gmail.com>
2025-06-29 00:06:02 -0400

    Showin grants

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
 -- Lists privileges of the MySQL users
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
-GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
