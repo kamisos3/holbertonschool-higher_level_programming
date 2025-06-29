8003236c23220ae7a5db8aaba8508d6f25e75a98
kamisos3 <kamilasostre11@gmail.com>
2025-06-29 00:02:59 -0400

    Granting user permissions

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,5 +2,8 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
+GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
