8668fbc5b3ae27edb369c9bca2d310ca45691c00
kamisos3 <kamilasostre11@gmail.com>
2025-06-30 19:46:50 -0400

    Prints states matching an argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = ("SELECT * FROM states WHERE name = '{}'"
+    query = ("SELECT * FROM states WHERE name = '{}' "
             "ORDER BY id ASC").format(state_name)
     cur.execute(query)
 
