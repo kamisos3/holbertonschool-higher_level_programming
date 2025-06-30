bbc25dcbf0ad055e4cf72c407b607aa485e49228
kamisos3 <kamilasostre11@gmail.com>
2025-06-30 16:49:29 -0400

    Function list stares with letter N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Lists states that start with 'N' """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+cur = db.cursor()
+
+ # Picks letter 'N' states in SQL to output
+cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+cur.close()
+db.close()
