0b634e524ca8fbf2fea5276dc8c0cda31826c0ba
kamisos3 <kamilasostre11@gmail.com>
2025-06-30 22:28:49 -0400

    Prints every city grouped by state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines city class linked to table city"""
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from model_state import Base
 
 class City(Base):
@@ -9,3 +10,5 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship("State", backref="cities")
