5bdb8b420c7e8c9a004508532d2e1cb8ee092eb3
kamisos3 <kamilasostre11@gmail.com>
2025-06-30 21:11:15 -0400

    Prints all cities in database

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,8 @@ from model_state import Base, State
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     user, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
